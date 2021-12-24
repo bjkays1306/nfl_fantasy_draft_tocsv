b5041ef67b5c0b0f47717e1d2b7fcbd3e939edf5
Richard Clegg <cleggr2@gmail.com>
2021-12-23 18:05:50 -0600

    Set player_id as Unique Key

--- create_draft_results.py
+++ create_draft_results.py
@@ -10,16 +10,18 @@ players = soup.findAll('a', class_='playerNameFull')
 
 draft_dict = {}
 for i in players:
+    player_id = int(i.attrs['href'].split('playerId=')[1])
     player_name = i.contents[0]
     draft_position = i.parent.parent.contents[0].contents[0].split('.')[0]
     draft_round = i.parent.parent.contents[0].parent.parent.parent.contents[0].contents[0].split(' ')[1]
     fantasy_team = i.parent.parent.contents[6].text
-    draft_dict.update({player_name: {"DraftPosition": draft_position,
-                                     "DraftRound": draft_round,
-                                     "FantasyTeam": fantasy_team}})
+    draft_dict.update({player_id: {"PlayerName": player_name,
+                                   "DraftPosition": draft_position,
+                                   "DraftRound": draft_round,
+                                   "FantasyTeam": fantasy_team}})
 
 with open('nfl_fantasy_draft_order.csv', 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(['PlayerName', 'DraftPosition', 'DraftRound', 'FantasyTeam'])  # Create Header
+    writer.writerow(['player_id', 'PlayerName', 'DraftPosition', 'DraftRound', 'FantasyTeam'])  # Create Header
     for k, v in draft_dict.items():
-        writer.writerow([k, v['DraftPosition'], v['DraftRound'], v['FantasyTeam']])
+        writer.writerow([k, v['PlayerName'], v['DraftPosition'], v['DraftRound'], v['FantasyTeam']])
