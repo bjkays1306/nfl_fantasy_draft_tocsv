2e95b854e0a9bea0913f7a62ed33ff5b43ae6620
Richard Clegg <cleggr2@gmail.com>
2021-12-23 18:50:25 -0600

    Add fantasy team ID

--- create_draft_results.py
+++ create_draft_results.py
@@ -15,13 +15,15 @@ for i in players:
     draft_position = i.parent.parent.contents[0].contents[0].split('.')[0]
     draft_round = i.parent.parent.contents[0].parent.parent.parent.contents[0].contents[0].split(' ')[1]
     fantasy_team = i.parent.parent.contents[6].text
+    fantasy_team_id = i.parent.parent.contents[5].attrs['class'][1].split('-')[1]
     draft_dict.update({player_id: {"PlayerName": player_name,
                                    "DraftPosition": draft_position,
                                    "DraftRound": draft_round,
-                                   "FantasyTeam": fantasy_team}})
+                                   "FantasyTeam": fantasy_team,
+                                   "FantasyTeamID": fantasy_team_id}})
 
 with open('nfl_fantasy_draft_order.csv', 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(['player_id', 'PlayerName', 'DraftPosition', 'DraftRound', 'FantasyTeam'])  # Create Header
+    writer.writerow(['player_id', 'PlayerName', 'DraftPosition', 'DraftRound', 'FantasyTeamID', 'FantasyTeam'])
     for k, v in draft_dict.items():
-        writer.writerow([k, v['PlayerName'], v['DraftPosition'], v['DraftRound'], v['FantasyTeam']])
+        writer.writerow([k, v['PlayerName'], v['DraftPosition'], v['DraftRound'], v['FantasyTeamID'], v['FantasyTeam']])
