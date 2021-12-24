9e751708f2247c62d0fd9d43145a5c724436db0a
Richard Clegg <cleggr2@gmail.com>
2021-12-23 19:54:10 -0600

    Refactoring

--- create_draft_results.py
+++ create_draft_results.py
@@ -1,29 +1,53 @@
-import config
 import csv
 from util import get_soup as gs
+import config
 
-league_id = config.LEAGUE_ID
+LEAGUE_ID = config.LEAGUE_ID
 
-soup = gs.get_draft_results(league_id)
 
-players = soup.findAll('a', class_='playerNameFull')
+def create_draft_results_dict(league_id):
+    soup = gs.get_draft_results(league_id)
 
-draft_dict = {}
-for i in players:
-    player_id = int(i.attrs['href'].split('playerId=')[1])
-    player_name = i.contents[0]
-    draft_position = i.parent.parent.contents[0].contents[0].split('.')[0]
-    draft_round = i.parent.parent.contents[0].parent.parent.parent.contents[0].contents[0].split(' ')[1]
-    fantasy_team = i.parent.parent.contents[6].text
-    fantasy_team_id = i.parent.parent.contents[5].attrs['class'][1].split('-')[1]
-    draft_dict.update({player_id: {"PlayerName": player_name,
-                                   "DraftPosition": draft_position,
-                                   "DraftRound": draft_round,
-                                   "FantasyTeam": fantasy_team,
-                                   "FantasyTeamID": fantasy_team_id}})
+    players = soup.findAll('a', class_='playerNameFull')
 
-with open('nfl_fantasy_draft_order.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(['player_id', 'PlayerName', 'DraftPosition', 'DraftRound', 'FantasyTeamID', 'FantasyTeam'])
-    for k, v in draft_dict.items():
-        writer.writerow([k, v['PlayerName'], v['DraftPosition'], v['DraftRound'], v['FantasyTeamID'], v['FantasyTeam']])
+    draft_dict = {}
+    for i in players:
+        player_id = int(i.attrs['href'].split('playerId=')[1])
+        player_name = i.contents[0]
+        draft_position = i.parent.parent.contents[0].contents[0].split('.')[0]
+        draft_round = i.parent.parent.contents[0].parent.parent.parent.contents[0].contents[0].split(' ')[1]
+        fantasy_team = i.parent.parent.contents[6].text
+        fantasy_team_id = i.parent.parent.contents[5].attrs['class'][1].split('-')[1]
+        draft_dict.update({player_id: {"PlayerName": player_name,
+                                       "DraftPosition": draft_position,
+                                       "DraftRound": draft_round,
+                                       "FantasyTeam": fantasy_team,
+                                       "FantasyTeamID": fantasy_team_id}})
+
+    return draft_dict
+
+
+def export_team_rosters_to_csv(draft_results: dict):
+    with open('nfl_fantasy_draft_order.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        # Create Header
+        writer.writerow(['player_id',
+                         'PlayerName',
+                         'DraftPosition',
+                         'DraftRound',
+                         'FantasyTeamID',
+                         'FantasyTeam'])
+        # Write Data
+        for k, v in draft_results.items():
+            writer.writerow(
+                [k,
+                 v['PlayerName'],
+                 v['DraftPosition'],
+                 v['DraftRound'],
+                 v['FantasyTeamID'],
+                 v['FantasyTeam']])
+
+
+def main():
+    draft_results_dict = create_draft_results_dict(LEAGUE_ID)
+    export_team_rosters_to_csv(draft_results_dict)
--- create_team_rosters.py
+++ create_team_rosters.py
@@ -1,38 +1,61 @@
-import config
 from util import get_soup as gs
 import csv
+import config
 
-league_id = config.LEAGUE_ID
+LEAGUE_ID = config.LEAGUE_ID
 
-league_settings_soup = gs.get_league_settings(league_id)
 
-league_settings = league_settings_soup.find_all('div')
+def get_number_of_teams(league_id: int):
+    league_settings_soup = gs.get_league_settings(league_id)
 
-# Get the number of Teams in the League from Settings
-for i in league_settings:
-    if i.previous == 'Teams':
-        Number_of_Teams = i.contents[0]
+    league_settings = league_settings_soup.find_all('div')
 
-team_id_numbers = range(1, int(Number_of_Teams) + 1)
+    # Get the number of Teams in the League from Settings
+    for i in league_settings:
+        if i.previous == 'Teams':
+            number_of_teams = i.contents[0]
 
-player_dict = {}
+            team_id_numbers = range(1, int(number_of_teams) + 1)
 
-for team_id_number in team_id_numbers:
-    team_page = gs.get_team_roster_by_team_id(league_id, team_id_number)
-    team_page_players = team_page.find_all('a', class_='playerCard')
+            return team_id_numbers
 
-    for i in team_page_players:
-        player_id = int(i.attrs['href'].split('playerId=')[1])
-        if str(i.contents[0]) != 'View News':
-            player_name = str(i.contents[0])
-        # draft_position = i.parent.parent.contents[0].contents[0].split('.')[0]
-        # draft_round = i.parent.parent.contents[0].parent.parent.parent.contents[0].contents[0].split(' ')[1]
-        # fantasy_team = i.parent.parent.contents[6].text
-        player_dict.update({player_id: {"PlayerName": player_name,
-                                        "TeamIdNumber": team_id_number}})
 
-with open('nfl_fantasy_player_rosters.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(['player_id', 'PlayerName', 'TeamIdNumber'])  # Create Header
-    for k, v in player_dict.items():
-        writer.writerow([k, v['PlayerName'], v['TeamIdNumber']])
+def create_team_rosters_dict(league_id: int, team_id_numbers: range):
+    player_dict = {}
+
+    for team_id_number in team_id_numbers:
+        team_page = gs.get_team_roster_by_team_id(league_id, team_id_number)
+        team_page_players = team_page.find_all('a', class_='playerCard')
+
+        for i in team_page_players:
+            player_id = int(i.attrs['href'].split('playerId=')[1])
+            if str(i.contents[0]) != 'View News':
+                player_name = str(i.contents[0])
+            # draft_position = i.parent.parent.contents[0].contents[0].split('.')[0]
+            # draft_round = i.parent.parent.contents[0].parent.parent.parent.contents[0].contents[0].split(' ')[1]
+            # fantasy_team = i.parent.parent.contents[6].text
+                player_dict.update({player_id: {"PlayerName": player_name,
+                                                "TeamIdNumber": team_id_number}})
+
+    return player_dict
+
+
+def export_team_rosters_to_csv(team_rosters):
+    with open('nfl_fantasy_player_rosters.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        # Create Header
+        writer.writerow(['player_id',
+                         'PlayerName',
+                         'TeamIdNumber'])
+        # Write Data
+        for k, v in team_rosters.items():
+            writer.writerow(
+                            [k,
+                             v['PlayerName'],
+                             v['TeamIdNumber']])
+
+
+def main():
+    id_numbers = get_number_of_teams(LEAGUE_ID)
+    team_rosters_dict = create_team_rosters_dict(LEAGUE_ID, id_numbers)
+    export_team_rosters_to_csv(team_rosters_dict)
