7e8db07021048bcf3b1e95ce4f16e37a5c5f4015
rclegg <cleggr2@gmail.com>
2022-05-23 23:53:22 -0500

    Minor Refactoring

--- create_draft_results.py
+++ create_draft_results.py
@@ -2,18 +2,20 @@ import csv
 import NFLSoup
 import config
 
+"""Creates a CSV of fantasy draft results from current draft or specified year."""
+
 LEAGUE_ID = config.LEAGUE_ID
 SEASON_END_YEAR = config.SEASON_END_YEAR
 
 nfls = NFLSoup.NFLFantasyFootballSoup(league_id=LEAGUE_ID, season_end_year=SEASON_END_YEAR)
 
 
-def create_draft_results_dict():
+def create_draft_results() -> dict:
     soup = nfls.get_draft_results()
 
     players = soup.findAll('a', class_='playerNameFull')
 
-    draft_dict = {}
+    draft_results = {}
     for i in players:
         player_id = int(i.attrs['href'].split('playerId=')[1])
         player_name = i.contents[0]
@@ -22,17 +24,17 @@ def create_draft_results_dict():
         fantasy_team = i.parent.parent.contents[6].contents[0].text
         fantasy_manager = i.parent.parent.contents[6].contents[1].text
         fantasy_team_id = i.parent.parent.contents[5].attrs['class'][1].split('-')[1]
-        draft_dict.update({player_id: {"PlayerName": player_name,
-                                       "DraftPosition": draft_position,
-                                       "DraftRound": draft_round,
-                                       "DraftingTeamName": fantasy_team,
-                                       "DraftingTeamManager": fantasy_manager,
-                                       "DraftingTeamId": fantasy_team_id}})
+        draft_results.update({player_id: {"PlayerName": player_name,
+                                          "DraftPosition": draft_position,
+                                          "DraftRound": draft_round,
+                                          "DraftingTeamName": fantasy_team,
+                                          "DraftingTeamManager": fantasy_manager,
+                                          "DraftingTeamId": fantasy_team_id}})
 
-    return draft_dict
+    return draft_results
 
 
-def export_team_rosters_to_csv(draft_results: dict):
+def export_draft_results_to_csv(draft_results: dict):
     with open('nfl_fantasy_draft_order.csv', 'w', newline='') as f:
         writer = csv.writer(f)
         # Create Header
@@ -56,5 +58,8 @@ def export_team_rosters_to_csv(draft_results: dict):
 
 
 def main():
-    draft_results_dict = create_draft_results_dict()
-    export_team_rosters_to_csv(draft_results_dict)
+    draft_results_dict = create_draft_results()
+    export_draft_results_to_csv(draft_results_dict)
+
+
+main()
