f1fb83924e3e628cb66beaa2e08a04bcb63c4e78
rclegg <cleggr2@gmail.com>
2022-06-05 19:35:27 -0500

    Added comments

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 LEAGUE_ID = '<7 Digit League ID Number>'    # string
-SEASON_END_YEAR = 2021                      # int Year as YYYY
+SEASON_END_YEAR = '<4 Digit Year> or None'  # int Year as YYYY; use None for current season
