491c52a43982607d037757528383d765f9c7644d
rclegg <cleggr2@gmail.com>
2023-05-25 00:13:57 -0500

    Add TypeHint

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-LEAGUE_ID = '<7 Digit League ID Number>'    # int
-SEASON_END_YEAR = '<4 Digit Year> or None'  # int Year as YYYY; use None for current season
+LEAGUE_ID: int = 0000000  # 7 Digit League ID Number
+SEASON_END_YEAR: int or None = 2023  # Year as YYYY; use None for current season
